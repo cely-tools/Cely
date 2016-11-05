4aff6dc8856f56867e57ae1629698f1b455edaf3
Fabian Buentello <fabian.buentello@gmail.com>
2016-11-05 15:24:10 -0500

    finish documentation

--- Cely Demo/User.swift
+++ Cely Demo/User.swift
@@ -62,21 +62,3 @@ extension User {
         return property.get()
     }
 }
-
-struct CottonCandy: CelyStyle {
-    func backgroundColor() -> UIColor {
-        return UIColor(red: 86/255, green: 203/255, blue: 249/255, alpha: 1) // Changing Color
-    }
-    func buttonTextColor() -> UIColor {
-        return .white
-    }
-    func buttonBackgroundColor() -> UIColor {
-        return UIColor(red: 253/255, green: 108/255, blue: 179/255, alpha: 1) // Changing Color
-    }
-    func textFieldBackgroundColor() -> UIColor {
-        return UIColor.white.withAlphaComponent(0.4)
-    }
-    func appLogo() -> UIImage? {
-        return UIImage(named: "CelyLogo")
-    }
-}
