8a73f5d8248c50212d53173392458e5cc6c0aba7
Fabian Buentello <fabian.buentello@gmail.com>
2017-06-15 09:16:09 -0500

    Add Locksmith copyright material (#20)

--- Sources/LocksmithAccessibleOption.swift
+++ Sources/LocksmithAccessibleOption.swift
@@ -1,3 +1,11 @@
+//
+//  LocksmithAccessibleOption.swift
+//  Cely
+//
+//  Created by Matthew Palmer on 7/31/16.
+//  Copyright © 2016 Matthew Palmer. All rights reserved.
+//
+
 import Foundation
 
 // MARK: Accessible
