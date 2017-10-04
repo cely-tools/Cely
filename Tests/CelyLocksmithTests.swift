716166bb0e6190d10ff1d8d95f34d4dae6d89306
Fabian Buentello <fbuentelloapp@gmail.com>
2017-10-04 07:03:37 -0500

    Update tests for Swift 4

--- Tests/CelyLocksmithTests.swift
+++ Tests/CelyLocksmithTests.swift
@@ -288,16 +288,16 @@ class LocksmithTests: XCTestCase {
                 ])
 
             let cr = dict[String(kSecAttrCreator)] as! CFNumber
-            XCTAssertEqual(cr as UInt, creator)
+			XCTAssertEqual(cr as! UInt, creator)
 
             let ty = dict[String(kSecAttrType)] as! CFNumber
-            XCTAssertEqual(ty as UInt, type)
+            XCTAssertEqual(ty as! UInt, type)
 
             let inv = dict[String(kSecAttrIsInvisible)] as! CFBoolean
-            XCTAssertEqual(inv as Bool, isInvisible)
+            XCTAssertEqual(inv as! Bool, isInvisible)
 
             let neg = dict[String(kSecAttrIsNegative)] as! CFBoolean
-            XCTAssertEqual(neg as Bool, isNegative)
+            XCTAssertEqual(neg as! Bool, isNegative)
 
             let gen = dict[String(kSecAttrGeneric)]
             XCTAssertNil(gen)
